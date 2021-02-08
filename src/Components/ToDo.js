b32ef7703f1c185e4bd5a50c3676494cc643f30a
ArturShahbazyan <arturshahbazyan777@yandex.ru>
2021-02-09 00:21:57 +0400

    AddNewTask component has been added, changes have been made in ToDo component

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 import Task from './Task'
+import AddNewTask from './AddNewTask'
 
 class ToDo extends Component {
     state = {
-        tasks:["Task1", "Task2", "Task3"]
+        tasks:["Task1", "Task2", "Task3"],
+        inputValue:''
+    }
+
+    handleCatchValue = (inputValue) => {
+        this.setState({
+            inputValue
+        })
     }
 
     render(){
+
+        const { inputValue } = this.state;
+
         return (
             <div>
-                <div className="add_task_col">
-                    <input type="text" placeholder="add a new todo..."/>
-                    <button>Add</button>
-                </div>
+                
+                <AddNewTask onSubmit={this.handleCatchValue}/>
+                <p>{inputValue}</p>
                 <div className="tasks_container">
                     {
                         this.state.tasks.map((task,index)=>{
