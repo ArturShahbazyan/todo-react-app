355bde1801ab27c5314fa212edf32b41e912f33a
ArturShahbazyan <arturshahbazyan777@yandex.ru>
2021-03-27 01:15:20 +0400

    Context Api

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import About from "./Components/pages/About/About";
 import Contact from "./Components/pages/Contact/Contact";
 import NotFound from "./Components/pages/NotFound/NotFound";
 import SingleTask from "./Components/pages/SingleTask/SingleTask";
+import ContactContextProvider from "./Context/ContactContextProvider";
 
 const pages = [
     {path: "/", component: ToDo},
-    {path: "/contact", component: Contact},
+    {path: "/contact", component: Contact, contextProvider: ContactContextProvider},
     {path: "/about", component: About},
     {path: "/task/:id", component: SingleTask},
     {path: "/404", component: NotFound},
@@ -21,6 +22,25 @@ class App extends Component {
     render() {
 
         const pageRoutes = pages.map((page, index) => {
+            if (page.path === '/contact') {
+                return <Route
+                    path={page.path}
+                    exact
+                    key={index}
+                    render={(props) => {
+                        return <>
+                            {
+                                <page.contextProvider>
+                                    <page.component {...props}/>
+                                </page.contextProvider>
+                            }
+                        </>
+                    }
+                    }
+                />
+
+            }
+
             return <Route
                 path={page.path}
                 component={page.component}
--- src/Components/pages/Contact/Contact.js
+++ src/Components/pages/Contact/Contact.js
@@ -1,14 +1,11 @@
 import ContactFormWithHooks from "../../ContactForm/ContactFormWithHooks";
-import ContactContextProvider from "../../../Context/ContactContextProvider";
 
 const Contact = () => {
     return (
-        <ContactContextProvider>
             <div>
                 <h2 className="text-center mb-4">Contact Page</h2>
                 <ContactFormWithHooks/>
             </div>
-        </ContactContextProvider>
     )
 }
 
